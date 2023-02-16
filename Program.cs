66a0877502ebf2b20d486f20071dd7b352966c00
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-16 14:09:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitCheckPoint1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Git checkpoint task 1.1");
+        }
+    }
+}
